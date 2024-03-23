a05c5cda12664cefe38251b33ea60bd7021b8d01
anasouardini <ouardinianas@proton.me>
2024-03-23 18:34:16 +0000

    feat: add nvm

--- anasouardini-installer.ts
+++ anasouardini-installer.ts
@@ -729,19 +729,20 @@ const steps: Steps[] = [
     ],
   },
   {
-    enabled: false,
     category: "common",
     title: "installing nvm and node",
     substeps: [
       {
         cmd: [
           `
-            curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash; \\
+            curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash; \\
             export NVM_DIR="$HOME/.nvm"; \\
             [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"; \\
             [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"; \\
-            nvm install node; \\
-            npm i -g pnpm && pnpm --force setup && source $HOME/.zshrc && pnpm i -g pnpm;
+            nvm i node; \\
+            nvm i 21; \\
+            corepack enable; \\
+            pnpm i -g pnpm;
           `,
         ],
       },
