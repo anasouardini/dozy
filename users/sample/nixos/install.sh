4c84fa3f3b13df0740a24f6371389883b0a3077d
anasouardini <ouardinianas@proton.me>
2024-06-13 01:07:33 +0100

    fix: fixed misssing installation command

--- users/sample/nixos/install.sh
+++ users/sample/nixos/install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+## config
+bootType="uefi"; # uefi/bios
+
 lsblk
 read -p "Insert the name of your disk (sda, sdb, etc):" DISK
 if [[ -z $DISK ]]; then
@@ -9,30 +12,51 @@ fi
 DISK="/dev/$DISK"
 
 echo "\n=================== Partitioning\n"
-sudo parted "$DISK" -- mklabel gpt
-sudo parted "$DISK" -- mkpart ESP fat32 1MiB 512MiB
-sudo parted "$DISK" -- set 3 boot on
-sudo parted "$DISK" -- mkpart primary 512MiB -12GiB
-sudo parted "$DISK" -- mkpart primary linux-swap -12GiB 100%
+if [[ $bootType == "uefi" ]]; then
+    parted "$DISK" -- mklabel gpt
+    parted "$DISK" -- mkpart root ext4 512MB -12GB
+    parted "$DISK" -- mkpart swap linux-swap -12GB 100%
+    parted "$DISK" -- mkpart ESP fat32 1MiB 512MiB
+    parted "$DISK" -- set 3 esp on
+else
+    parted "$DISK" -- mklabel msdos
+    parted "$DISK" -- mkpart primary ext4 1MB -12GB
+    parted "$DISK" -- set 1 boot on
+    parted "$DISK" -- mkpart primary linux-swap -12GB 100%
+fi
 
 echo "\n=================== Formatting\n"
-sudo mkfs.fat -F 32 -n boot "${DISK}1"
-sudo mkfs.ext4 -L nixos "${DISK}2"
-sudo mkswap -L swap "${DISK}3"
+mkfs.ext4 -L nixos "${DISK}1"
+mkswap -L swap "${DISK}2"
+if [[ $bootType == "uefi" ]]; then
+    mkfs.fat -F 32 -n boot "${DISK}3"
+fi
 
 echo "\n=================== Installing\n"
-sudo mount /dev/disk/by-label/nixos /mnt
-sudo mkdir -p /mnt/boot
-sudo mount /dev/disk/by-label/boot /mnt/boot
-sudo swapon "${DISK}${NAME_DIVIDER}3"
+mount /dev/disk/by-label/nixos /mnt
+if [[ $bootType == "uefi" ]]; then
+    mkdir -p /mnt/boot
+    # mount /dev/disk/by-label/boot /mnt/boot
+    mount -o umask=077 /dev/disk/by-label/boot /mnt/boot
+fi
+swapon "${DISK}${NAME_DIVIDER}2"
 
 echo "\n=================== Generating config files\n"
 sudo nixos-generate-config --root /mnt
 
-echo "\n=================== Preparing the chroot environment\n"
-sudo mount --bind /proc /mnt/proc
-sudo mount --bind /dev /mnt/dev
-sudo mount --bind /sys /mnt/sys
-sudo cp /etc/resolv.conf /mnt/etc/resolv.conf # dns might not be sat correctly (it's a common problem)
-chroot /mnt /nix/var/nix/profiles/system/activate
-chroot /mnt /run/current-system/sw/bin/bash
+echo "\n=================== final steps\n"
+if [[ ! $bootType == "uefi" ]]; then
+    ## set `boot.loader.grub.device = true`
+fi
+nixos-install --no-root-passwd
+
+# echo "\n=================== Preparing the chroot environment\n"
+# sudo mount --bind /proc /mnt/proc
+# sudo mount --bind /dev /mnt/dev
+# sudo mount --bind /sys /mnt/sys
+# sudo cp /etc/resolv.conf /mnt/etc/resolv.conf # dns might not be sat correctly (it's a common problem)
+# chroot /mnt /nix/var/nix/profiles/system/activate
+# chroot /mnt /run/current-system/sw/bin/bash
+## todo: run in chroot
+## passwd
+## adduser venego
