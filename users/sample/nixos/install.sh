efe7f124f66aeeb84526452fd6512c64f8b846a0
anasouardini <ouardinianas@proton.me>
2024-06-13 02:46:10 +0100

    minor: minor

--- users/sample/nixos/install.sh
+++ users/sample/nixos/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ## config
-bootType="uefi"; # uefi/bios
+bootType="bios"; # uefi/bios
 
 setfont ter-v22n
 
@@ -16,15 +16,15 @@ DISK="/dev/$DISK"
 printf "\n=================== Partitioning\n"
 if [[ $bootType == "uefi" ]]; then
     sudo parted "$DISK" -- mklabel gpt
-    sudo parted "$DISK" -- mkpart root ext4 512MB -12GB
-    sudo parted "$DISK" -- mkpart swap linux-swap -12GB 100%
+    sudo parted "$DISK" -- mkpart root ext4 512MB -12GiB
+    sudo parted "$DISK" -- mkpart swap linux-swap -12GiB 100%
     sudo parted "$DISK" -- mkpart ESP fat32 1MiB 512MiB
     sudo parted "$DISK" -- set 3 esp on
 else
     sudo parted "$DISK" -- mklabel msdos
-    sudo parted "$DISK" -- mkpart primary ext4 1MB -12GB
+    sudo parted "$DISK" -- mkpart primary ext4 1MB -12GiB
     sudo parted "$DISK" -- set 1 boot on
-    sudo parted "$DISK" -- mkpart primary linux-swap -12GB 100%
+    sudo parted "$DISK" -- mkpart primary linux-swap -12GiB 100%
 fi
 
 printf "\n=================== Formatting\n"
@@ -34,7 +34,7 @@ if [[ $bootType == "uefi" ]]; then
     sudo mkfs.fat -F 32 -n boot "${DISK}3"
 fi
 
-printf "\n=================== Installing\n"
+printf "\n=================== Mounting\n"
 sudo mount /dev/disk/by-label/nixos /mnt
 if [[ $bootType == "uefi" ]]; then
     sudo mkdir -p /mnt/boot
@@ -48,7 +48,7 @@ sudo nixos-generate-config --root /mnt
 sudo mv /mnt/etc/nixos/configuration.nix /mnt/etc/nixos/configuration.nix.bak
 sudo curl -o /mnt/etc/nixos/configuration.nix https://postinstaller.netlify.app/users/sample/nixos/configuration.nix
 
-printf "\n=================== final steps\n"
+printf "\n=================== Final Steps\n"
 if [[ $bootType == "uefi" ]]; then
     ## for Grub
     # boot.loader.grub.device="nodev";
@@ -63,8 +63,12 @@ else
     # boot.loader.grub.useOSProber=true;
     printf "" # can't do empty blocks in bash
 fi
+
+printf "\n=================== Installing\n"
 sudo nixos-install --no-root-passwd
 
+# Might not be needed since most things can be done by
+# just dropping a config file to the target disk ¯\_(ツ)_/¯
 # printf "\n=================== Preparing the chroot environment\n"
 # sudo mount --bind /proc /mnt/proc
 # sudo mount --bind /dev /mnt/dev
@@ -72,6 +76,3 @@ sudo nixos-install --no-root-passwd
 # sudo cp /etc/resolv.conf /mnt/etc/resolv.conf # dns might not be sat correctly (it's a common problem)
 # chroot /mnt /nix/var/nix/profiles/system/activate
 # chroot /mnt /run/current-system/sw/bin/bash
-## todo: run in chroot
-## passwd
-## adduser venego
