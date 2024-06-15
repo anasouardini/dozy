4446cc79907f9c19dcd48d4fe730f666258c8e1b
anasouardini <ouardinianas@proton.me>
2024-06-15 09:16:22 +0100

    feat: dynamically setting boot type

--- users/sample/arch/install.sh
+++ users/sample/arch/install.sh
@@ -2,6 +2,11 @@
 
 ## config
 bootType="bios"; # uefi/bios
+if [[ -d /sys/firmware/efi ]];then
+    bootType="uefi"; # uefi/bios
+else
+    bootType="bios"; # uefi/bios
+fi
 username="venego";
 initialPassword="venego";
 hostname="i5";
