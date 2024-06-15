8be6c92b7c430542975c6c87d3708f412c2c8dc9
anasouardini <ouardinianas@proton.me>
2024-06-15 07:58:52 +0100

    feat: add a post-installation script

---
+++ users/sample/arch/post-install.sh
@@ -0,0 +1,11 @@
+cd;
+
+sudo pacman -S xorg-xinit i3-wm networkmanager --noconfirm;
+sudo systemctl enable NetworkManager;
+
+echo 'exec i3' >> .xinitrc;
+cat << EOF | tee bash_profile;
+if [[ $(tty) == "/dev/tty1" ]]; then
+	pgrep i3 || startx &
+fi
+EOF
