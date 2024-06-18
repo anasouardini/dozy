0fd766b47885d8eab9c4dc946307da0065045589
anasouardini <ouardinianas@proton.me>
2024-06-18 16:46:41 +0100

    fix: check if uzip installed before installing deno

--- dist/users/anasouardini/arch/dropper.sh
+++ dist/users/anasouardini/arch/dropper.sh
@@ -53,11 +53,11 @@ if [ -f $installerPath ]; then
   rm -rf $installerPath;
 fi
 
-curl -fsSL "${host}/users/${githubUsername}/installer.ts" -o $installerPath;
 echo "running the post-installation script";
 type unzip > /dev/null 2>&1;
 if [ ! $? -eq 0 ]; then
   echo "installing unzip...";
   sudo pacman -S unzip --noconfirm
 fi
+curl -fsSL "${host}/users/${githubUsername}/installer.ts" -o $installerPath;
 $HOME/.deno/bin/deno run --allow-all $installerPath $installerArgs;
