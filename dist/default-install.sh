10a8d2bd9f86b5133721d6a85a13a00608ef22de
anasouardini <ouardinianas@proton.me>
2025-02-06 01:33:35 +0100

    enhancement: shortning the url of default installer

---
+++ dist/default-install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+host="https://dozy.netlify.app";
+
+githubUsername='anasouardini';
+installerArgs=$@;
+
+function installIfDoesNotExist(){
+  for package in "$@"; do
+    command -v "$package" > /dev/null 2>&1;
+    if [ ! $? -eq 0 ]; then
+      echo "installing ${package}...";
+      sudo apt install $package -y
+    fi
+  done
+}
+
+# deno needs unzip
+installIfDoesNotExist curl wget unzip
+
+#-------- install deno
+ls $HOME/.deno/bin/deno > /dev/null 2>&1;
+if [ ! $? -eq 0 ]; then
+  echo "running the deno installer";
+  curl -fsSL https://deno.land/x/install/install.sh | sh
+  echo 'PATH="$PATH:$HOME/.deno/bin/"' | tee -a .bashrc;
+  export 'PATH="$PATH:$HOME/.deno/bin/"';
+fi
+
+#------------ run the installer (TS script)
+echo "download post-installation script";
+installerPath="./${githubUsername}-installer.ts";
+if [ -f $installerPath ]; then
+  echo "The file: '${installerPath}' is going to be removed.";
+  read -p "\e[31mCtrl+C\e[0m to cancel or \e[32mEnter\e[0m to continue: " dummy;
+  rm -rf $installerPath;
+fi
+
+wget -O $installerPath "${host}/users/${githubUsername}/debian/installer.ts";
+echo "running the post-installation script";
+$HOME/.deno/bin/deno run --allow-all $installerPath $installerArgs;
